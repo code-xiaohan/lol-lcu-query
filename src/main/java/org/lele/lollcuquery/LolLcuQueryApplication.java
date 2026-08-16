6e626d9f468d1080c09f689fea53e661962914bc
hanzongle <sdtahzl@163.com>
2026-08-16 11:54:21 +0800

    first commit

---
+++ src/main/java/org/lele/lollcuquery/LolLcuQueryApplication.java
@@ -0,0 +1,13 @@
+package org.lele.lollcuquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolLcuQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolLcuQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lele/lollcuquery/LolLcuQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lele.lollcuquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolLcuQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
